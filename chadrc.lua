3ea7ddecc51a093dee649c22d80d302b0d8afc9b
dreknix <dreknix@proton.me>
2023-11-21 13:51:56 +0100

    Enable netrw plugin to use  command

--- chadrc.lua
+++ chadrc.lua
@@ -31,4 +31,30 @@ M.ui = {
 M.plugins = 'custom.plugins'
 M.mappings = require 'custom.mappings'
 
+-- load default list of disabled plugins in NvChad
+local default_disabled_plugins = require('plugins.configs.lazy_nvim').performance.rtp.disabled_plugins
+
+-- specify which plugins should be removed from default disabled list
+local enabled = {
+  netrwPlugin = true,
+}
+-- enable those plugins
+local i=1
+while i <= #default_disabled_plugins do
+    if enabled[default_disabled_plugins[i]] then
+        table.remove(default_disabled_plugins, i)
+    else
+        i = i + 1
+    end
+end
+-- print(vim.inspect(default_disabled_plugins))
+
+M.lazy_nvim = {
+  performance = {
+    rtp = {
+      disabled_plugins = default_disabled_plugins,
+    },
+  },
+}
+
 return M
